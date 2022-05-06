e50d0dc9b4dafab9a9681603a90a1b118fcf5a5c
MadManMoon <madmanmoon@gmail.com>
2022-05-06 13:23:28 +0100

    Create main.js
    This is just a start, it's includes `$(".madmanmoon").remove;` which is basically what I want to happen to any elements created with that ID.

---
+++ main.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Remove MMM IDs
+// @namespace   MMM Scripts
+// @match       *://*/*
+// @grant       none
+// @version     0.1
+// @author      mmm
+// @require     https://code.jquery.com/jquery-3.6.0.min.js
+// @description 06/05/2022, 12:55:13
+// @run-at      document-start
+// ==/UserScript==
+
+/* This needs a mutation observer, but I literally have zero clue here */
+$(".madmanmoon").remove;
